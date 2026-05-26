1dd57df2a6fdb44eed430bb4075ba11edd7d5fc0
Amrit Kumar <satishkumar12700@gmail.com>
2026-05-26 08:59:11 +0530

    Doing the initial commits

---
+++ multiple_regression.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib as plt
+# Features:[size, bedrooms, age]
+X_train = np.array([
+    [850,2,20],
+    [900,2,18],
+    [1200,3,15],
+    [1500,3,10],
+    [1700,4,8],
+    [2000,4,5],
+    [2200,5,4],
+    [2500,5,3],
+    [2700,5,2],
+    [3000,6,1]
+])
+# House prices
+y_train = np.array([
+    180,
+    200,
+    280,
+    350,
+    420,
+    500,
+    580,
+    670,
+    730,
+    820
+])
+
